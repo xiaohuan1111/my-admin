39d5fa354a1bdcaa4f7928f7d6a75d1a380cbb46
huan.zhang@wantsea.com <ZhangHuan>
2017-08-14 18:12:08 +0800

    table addForm

--- src/components/nav1/table.vue
+++ src/components/nav1/table.vue
@@ -7,10 +7,10 @@
 		 			<el-input placeholder="姓名" v-model="filters.name"></el-input>
 		 		</el-form-item>
 		 		<el-form-item>
-		 			<el-button type="primary">查询</el-button>
+		 			<el-button type="primary" @click="getUsers">查询</el-button>
 		 		</el-form-item>
 		 		<el-form-item>
-		 			<el-button type="primary">新增</el-button>
+		 			<el-button type="primary" @click="handleAdd">新增</el-button>
 		 		</el-form-item>
 		 	</el-form>
 		 </el-col>
@@ -35,6 +35,34 @@
 		 	<el-button type="danger">批量删除</el-button>
 		 	<el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="20" :total="total" style="float:right;"></el-pagination>
 		 </el-col>
+
+		 <!-- 对话框 -->
+		 <el-dialog title="新增" v-model="addFormVisible" :close-on-click-model="false" style="text-align:left;">
+		 	<el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+		 		<el-form-item label="姓名" prop="name">
+		 			<el-input v-model="addForm.name" auto-complete="off"></el-input>
+		 		</el-form-item>
+		 		<el-form-item label="性别">
+		 			<el-radio-group v-model="addForm.sex">
+		 				<el-radio :label='1'>男</el-radio>
+		 				<el-radio :label='0'>女</el-radio>
+		 			</el-radio-group>
+		 		</el-form-item>
+		 		<el-form-item label="年龄">
+		 			<el-input-number v-model="addForm.age" :min="0" :max="200"></el-input-number>
+		 		</el-form-item>
+		 		<el-form-item label="生日">
+		 			<el-date-picker type="date" v-model="addForm.birth" placeholder="请选择日期"></el-date-picker>
+		 		</el-form-item>
+		 		<el-form-item label="地址">
+		 			<el-input typp="textarea" v-model="addForm.addr"></el-input>
+		 		</el-form-item>
+		 	</el-form>
+		 	<div slot="footer" class="dialog-footer">
+		 		<el-button @click="addFormVisible = false">取消</el-button>
+		 		<el-button type="primary" @click="addsubmit('addForm')" :loading="addloading">确定</el-button>
+		 	</div>
+		 </el-dialog>
 	</div>
 </template>
 <script>
@@ -48,7 +76,21 @@
 				users: [],
 				total: 0,
 				page: 1,
-				listLoading: false
+				listLoading: false,
+				addFormVisible: false, // 新增界面是否显示
+				addloading: false,
+				addForm: {
+					name: '',
+					sex: -1,
+					age: 0,
+					birth: '',
+					addr: ''
+				},
+				addFormRules: {
+					name:[
+						{ required: true, message: '请输入姓名', trigger: 'blur' }
+					]
+				}
 			}
 		},
 		methods: {
@@ -65,7 +107,7 @@
         		this.page = val ;
         		this.getUsers();
       		},
-			//获取用户列表
+			// 获取用户列表
 			getUsers(){
 				let para = {
 					page: this.page,
@@ -78,7 +120,31 @@
 					this.users = res.data.users;
 					this.listLoading = false;
 				})
+			},
+			// 新增用户
+
+			handleAdd: function(){
+				this.addFormVisible = true
+			},
+			addsubmit: function(formname){
+				this.$refs[formname].validate((valid) =>{
+					if(valid){
+						this.$confirm('确认提交吗？','提示',{}).then(()=>{
+							this.loading = true,
+							this.$message({
+								type: 'success',
+								message: '新增成功！'
+							})
+						}).catch(()=>{
+							this.$message({
+								type:'info',
+								message: '新增失败！'
+							})
+						})
+					}
+				})
 			}
+
 		},
 		mounted(){
 			this.getUsers();
